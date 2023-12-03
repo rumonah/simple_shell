3ad136f9a21acbd50abf1d2c024002d4720125f5
rumonah <rummaloba@gmail.com>
2023-12-03 06:57:55 -0800

    added

--- fork.c
+++ fork.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - fork
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	pid_t pid;
+	pid_t ppid;
 
 	printf("Before fork I was twenty\n");
 
@@ -16,6 +22,17 @@ int main(void)
 	}
 
 	printf("After fork I became twenty six\n");
+
+	if (pid == 0)
+	{
+		sleep(10);
+		printf("My child\n");
+	}
+	else
+	{
+		ppid = getpid();
+		printf("The Mother pid is: %u\n", ppid);
+	}
 
 	return (0);
 }
