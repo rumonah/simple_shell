5206378b74ce208cda3c9658e18a1ed6c8f8c54b
rumonah <rummaloba@gmail.com>
2023-12-06 13:30:12 -0800

    command line

--- cmd_line_args.c
+++ cmd_line_args.c
@@ -14,7 +14,7 @@ int main(void)
 	char *cmd = NULL, *cmd_cpy = NULL, *token = NULL;
 	char *delim = " \n";
 	size_t r = 0;
-	int argc, t = 0;
+	int argc = 0, t = 0;
 	char **argv = NULL;
 
 	printf("$ ");
@@ -24,13 +24,30 @@ int main(void)
 	cmd_cpy = strdup(cmd);
 	token = strtok(cmd, delim);
 
-	while (cmd[t])
+	while (token)
 	{
-		if (cmd[t] == '\n')
-			printf("\\n ---> Input has %ld characters", strlen(cmd));
-		else
-			printf("%c", cmd[t]);
+		token = strtok(NULL, delim);
+		argc++;
+	}
+
+	printf("%d\n", argc);
+
+	argv = malloc(sizeof(char *) * argc);
+	token = strtok(cmd_cpy, delim);
+
+	while (token)
+	{
+		argv[t] = token;
+		token = strtok(NULL, delim);
 		t++;
 	}
+	argv[t] = NULL;
+
+	t = 0;
+
+	while (argv[t])
+		printf("%s\n", argv[t++]);
+	free(cmd), free(cmd_cpy), free(argv);
+
 	return (0);
 }
